b16883cdcfb1aecae54072b1e82ecf66d489376e
Manshiii15 <141203967+Manshiii15@users.noreply.github.com>
2024-02-25 14:34:12 +0530

    profile.css+enroll.css+registration.css

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import User_login from './userlogin';
 import Signup from './signup';
 
 import { Dashboard } from './dashboard';
+
 import Admin_login from './adminlogin';
 
 
+import {Enroll} from './enroll'
+import {Profile} from './profile'
+import {VoterRegistration} from './registeration'
 
 
 function App() {
   return (
     <>
+
     <Mainpage />
     <Signup />
     <User_login/>
     <Admin_login/>
+
+    {/* <Mainpage />
+    <Signup /> */}
+    {/* <Dashboard/> */}
+    {/* <Enroll/> */}
+    {/* <Profile/> */}
+    <VoterRegistration/>
+
+
     </>
   );
 }
